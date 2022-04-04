728db61e3ca070c8329fc3c9063da6ee3531253c
Yadin Yafe <yadin258@yahoo.com>
2022-04-04 19:44:58 +0300

    Add fields validation

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,5 +1,18 @@
-import { Link, redirect } from "remix"
+import { Link, redirect, useActionData, json } from "remix"
 import { db } from '~/utils/db.server'
+
+
+const validateTitle = (title) => {
+    if (typeof title !== 'string' || title.length < 3) {
+        return 'Title to short, needs to be at least 4 characters';
+    }
+}
+
+const validateBody = (body) => {
+    if (typeof body !== 'string' || body.length < 10) {
+        return 'Body to short, needs to be at least 10 characters';
+    }
+}
 
 // In this action method we get when user submit the form, this is Remix hook.
 export const action = async ({ request }) => { // In requrest we get can get user data and more.
@@ -9,11 +22,24 @@ export const action = async ({ request }) => { // In requrest we get can get use
 
     const formFields = { title, body };
 
+    const fieldErrors = {
+        title: validateTitle(title),
+        body: validateBody(body),
+    }
+
+    // If some of the values change we get and check if there are error so we return 400 code error.
+    if (Object.values(fieldErrors).some(Boolean)) {
+        console.log(fieldErrors);
+        return json({ fieldErrors, formFields }, { status: 400 })
+    }
+
     const post = await db.post.create({ data: formFields });
     return redirect(`/posts/${post.id}`); // At the end, when post is added we redirect user to Post list using redirect hook from Remix.
 }
 
 function NewPost() {
+    const actionData = useActionData();
+
     return (
         <>
             <div className="page-header">
@@ -28,10 +54,18 @@ function NewPost() {
                     <div className="form-control">
                         <label htmlFor='title'>Title</label>
                         <input type='text' name='title' id="title"></input>
+                        <div className="error">
+                            <p>{actionData?.fieldErrors?.title &&
+                                actionData?.fieldErrors?.title}</p>
+                        </div>
                     </div>
                     <div className="form-control">
                         <label htmlFor='body'>Body</label>
                         <textarea name='body' id="body"></textarea>
+                        <div className="error">
+                            <p>{actionData?.fieldErrors?.body &&
+                                actionData?.fieldErrors?.body}</p>
+                        </div>
                     </div>
                     <button type='submit' className="btn btn-block">Add Post!</button>
                 </form>
